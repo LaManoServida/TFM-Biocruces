28ca8abc73a87779b344befc6e52ae3a008edbec
Andoni <andoni7@outlook.com>
2020-05-15 15:44:08 +0200

    Script para reemplazar NA

---
+++ imputar_na.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+''' Reemplaza los valores perdidos por la media de la fila o columna '''
+
+# PARÁMETROS
+ruta = 'D:/Dropbox/UNI/TFM/datos/UPDRS 3.csv'
+subset_col = slice(3, -1)  # columnas a tener en cuenta [desde, hasta)
+media_de_fila = True  # imputar valores por la media de la fila
+
+# leer tabla
+tabla_entera = pd.read_csv(ruta, sep=',')
+tabla = tabla_entera.iloc[:, subset_col]  # solo las columnas seleccionadas
+
+if media_de_fila:
+    nueva = tabla.T.fillna(round(tabla.mean(axis=1))).T
+else:
+    nueva = tabla.fillna(round(tabla.mean()))
+
+# reconstruir la tabla entera
+tabla_entera.iloc[:, subset_col] = nueva
+
+# guardar
+ruta_split = os.path.splitext(ruta)
+tabla_entera.to_csv(ruta_split[0] + '_sin_na' + ruta_split[1], index=False)

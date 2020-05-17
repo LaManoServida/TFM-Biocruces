a29282d20da4eb87492776001b073fa811eeb2c4
Andoni <andoni7@outlook.com>
2020-05-17 02:24:11 +0200

    Acabado el sistema de eliminación de duplicados

--- unificar_registros.py
+++ unificar_registros.py
@@ -1,25 +1,39 @@
+import os
 import pandas as pd
-from funciones import buscar_csv
+from funciones import buscar_csv, guardar_tabla
 
 ''' Descarta los registros diferentes del mismo paciente, cogiendo el más completo o reciente '''
 
 # PARÁMETROS
-ruta_carpeta = 'D:/Dropbox/UNI/TFM/datos/modificado/'
+ruta_carpeta = 'D:/Dropbox/UNI/TFM/datos/'
 clave_principal = 'PATNO'
+clave_fecha = 'FECHA'  # debe ser un número, como UNIX timestamp
 
-# POR CADA TABLA DE DATOS
-for ruta_archivo in buscar_csv(ruta_carpeta):
-    tabla = pd.read_csv(ruta_archivo, sep=',')
+# leer cada tabla y quitar los duplicados
+for ruta_arch in buscar_csv(ruta_carpeta):
+    tabla = pd.read_csv(ruta_arch, sep=',')
 
-    # TODO
+    # quitar filas totalmente duplicadas
+    tabla.drop_duplicates(inplace=True)
+
     # si existen varios registros del mismo paciente:
-    #     si hay un registro con menos valores perdidos:
-    #         descartar los demás
-    #     si no (todos tienen el mismo número de valores perdidos):
-    #         si la tabla tiene una columna de fecha (porque no está ordenada cronológicamente):
-    #             descartar los registros anteriores de cada paciente
-    #         para el resto (y para las anteriores si todavía hay duplicidad con la misma fecha)
-    #         coger el último registro de cada paciente 
+    if not tabla[clave_principal].is_unique:
+        # coger lista de grupos de registros que tienen el mismo paciente
+        lista_rep = [grupo for _, grupo in tabla.groupby(clave_principal) if len(grupo) > 1]  # lista de DataFrames
 
-    # guardar cambios sobrescribiendo
-    tabla.to_csv(ruta_archivo, index=False)
+        # quitar todos los duplicados de la tabla para luego meter un único registro por paciente
+        tabla.drop_duplicates(subset=clave_principal, keep=False, inplace=True)
+
+        for grupo in lista_rep:
+            # quedarse con los registros con menos valores perdidos
+            num_perdidos = grupo.T.isna().sum().values  # array de número valores perdidos de cada registro
+            grupo = grupo[num_perdidos == num_perdidos.min()]
+
+            # de ellos elegir el último (se supone que es el más reciente)
+            # pero si no está ya ordenado y tiene una fecha
+            if clave_fecha in grupo:
+                grupo = grupo.sort_values(clave_fecha)  # se ordenan
+
+            tabla = pd.concat((tabla, grupo.iloc[[-1]]))  # añadir el último
+
+    guardar_tabla(tabla, ruta_arch, '_sin_duplicados')

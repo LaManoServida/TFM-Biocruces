3de87d495fa5d3c786dabf9e278694ac5a25ccf2
Andoni <andoni7@outlook.com>
2020-05-17 02:20:50 +0200

    Añadido guardar_tabla()

--- funciones.py
+++ funciones.py
@@ -1,6 +1,12 @@
 import os
 
 
-def buscar_csv(ruta):
-    """ Dada una ruta de una carpeta, devuelve una lista de archivos csv contenidos en ella """
-    return [ruta + arch for arch in os.listdir(ruta) if arch.endswith('.csv')]
+def buscar_csv(ruta_carpeta):
+    """ Dada una ruta de una carpeta, devuelve las rutas de los archivos csv contenidos en ella """
+    return [os.path.join(ruta_carpeta, arch) for arch in os.listdir(ruta_carpeta) if arch.endswith('.csv')]
+
+
+def guardar_tabla(tabla, ruta, sufijo):
+    """ Guarda la tabla en la ruta indicada """
+    ruta_split = os.path.splitext(ruta)
+    tabla.to_csv(ruta_split[0] + sufijo + ruta_split[1], index=False)

726c5aa39b5ae78e384ca6b35bb58f13d6ae1445
Andoni <andoni7@outlook.com>
2020-05-27 16:22:05 +0200

    Añadido valor por defecto al parámetro sufijo

--- preprocesamiento/funciones.py
+++ preprocesamiento/funciones.py
@@ -6,7 +6,7 @@ def buscar_csv(ruta_carpeta):
     return [os.path.join(ruta_carpeta, arch) for arch in os.listdir(ruta_carpeta) if arch.endswith('.csv')]
 
 
-def guardar_tabla(tabla, ruta, sufijo):
+def guardar_tabla(tabla, ruta, sufijo=''):
     """ Guarda la tabla en la ruta indicada """
     ruta_split = os.path.splitext(ruta)
     tabla.to_csv(ruta_split[0] + sufijo + ruta_split[1], index=False)

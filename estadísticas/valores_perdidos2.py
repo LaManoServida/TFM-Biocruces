4f37f6e3faec8f20efbe7ea2061c744dde7ec1ca
Andoni <andoni7@outlook.com>
2020-06-10 01:22:40 +0200

    Mostrar porcentajes de los valores NO perdidos

--- estadísticas/valores_perdidos2.py
+++ estadísticas/valores_perdidos2.py
@@ -4,9 +4,8 @@ import pandas as pd
 ''' Calcula estadísticas referentes a los valores perdidos de un archivo '''
 
 # PARÁMETROS
-ruta_archivo = 'D:/Dropbox/UNI/TFM/datos/7 - Juntar en un solo archivo/IDIOPATHIC PD/fusionado.csv'
+ruta_archivo = 'D:/Dropbox/UNI/TFM/datos/7 - Juntar en un solo archivo/RBD/fusionado.csv'
 nombre_informe = 'Informe valores perdidos.txt'
-clave_principal = 'PATNO'
 
 # leer tabla
 tabla = pd.read_csv(ruta_archivo, sep=',', float_precision='round_trip')
@@ -21,21 +20,18 @@ f = open(os.path.join(os.path.dirname(ruta_archivo), nombre_informe), mode='w', 
 f.write(
     f'VALORES PERDIDOS DE {ruta_archivo}\n'
     f'Número de filas: {num_filas}\n\n'
+    f'{"VARIABLE":17s}DATOS_CON_VALOR\n'
 )
 
 # rellenarlo por cada variable
 for variable in tabla.columns:
     # número de valores perdidos de esa variable
-    na_variable = tabla[variable].isna().sum()
+    datos_no_perdidos = num_filas - tabla[variable].isna().sum()
 
     # porcentaje
-    porcent_na_variable = round(na_variable / num_filas * 100, 1)
+    porcent_na_variable = round(datos_no_perdidos / num_filas * 100, 1)
 
     # escribir en el informe
-    f.write(
-        f'{str(variable):17s}'
-        f'Datos: {str(num_filas - na_variable):7s}'
-        f'NA: {str(na_variable):5s}({porcent_na_variable}%)\n'
-    )
+    f.write(f'{str(variable):17s}{str(datos_no_perdidos):7s}({porcent_na_variable}%)\n')
 
 f.close()

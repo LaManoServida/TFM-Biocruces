23d4fdcdc430ed081cbc026aa421ce8f8973cbb2
Andoni <andoni7@outlook.com>
2020-06-10 14:20:35 +0200

    Añadida funcionalidad de excluir variables

--- estadísticas/valores_perdidos2.py
+++ estadísticas/valores_perdidos2.py
@@ -1,25 +1,35 @@
 import os
+import numpy as np
 import pandas as pd
 
 ''' Calcula estadísticas referentes a los valores perdidos de un archivo '''
 
 # PARÁMETROS
-ruta_archivo = 'D:/Dropbox/UNI/TFM/datos/7 - Juntar en un solo archivo/RBD/fusionado.csv'
+ruta_archivo = 'D:/Dropbox/UNI/TFM/datos/7 - Juntar en un solo archivo/IDIOPATHIC PD/fusionado.csv'
 nombre_informe = 'Informe valores perdidos.txt'
-
-# leer tabla
-tabla = pd.read_csv(ruta_archivo, sep=',', float_precision='round_trip')
-
-# numero de filas
-num_filas = len(tabla)
+excluir_variables = ['TMTA', 'TMTA_tiempo', 'TMTB', 'TMTB_tiempo']
 
 # crear archivo del informe
 f = open(os.path.join(os.path.dirname(ruta_archivo), nombre_informe), mode='w', encoding='UTF-8')
 
+# leer tabla
+tabla = pd.read_csv(ruta_archivo, sep=',', float_precision='round_trip')
+
+# excluir las variables indicadas en parámetros
+if excluir_variables and isinstance(excluir_variables, list):
+    tabla.drop(excluir_variables, 1, inplace=True)
+
+# numero de filas
+num_filas = len(tabla)
+
+# numero de filas con algún valor perdido
+filas_con_na = np.count_nonzero(tabla.T.isna().sum().values)
+
 # escribir la cabecera
 f.write(
     f'VALORES PERDIDOS DE {ruta_archivo}\n'
-    f'Número de filas: {num_filas}\n\n'
+    f'Número de filas: {num_filas}\n'
+    f'Filas con algún valor perdido: {filas_con_na} ({round(filas_con_na / num_filas * 100, 1)}%)\n\n'
     f'{"VARIABLE":17s}DATOS_CON_VALOR\n'
 )
 

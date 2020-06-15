9df10a27ecd0af54d09a36bfd84fd4b7ea08c823
Andoni <andoni7@outlook.com>
2020-06-16 01:48:44 +0200

    Mejorado el procesamiento de la fecha y ahora actúa sobre la carpeta

--- preprocesamiento/fecha_a_timestamp.py
+++ preprocesamiento/fecha_a_timestamp.py
@@ -1,29 +1,42 @@
 import time
-from datetime import datetime as dt
+from dateutil.parser import parse
+import calendar
+import locale
 import pandas as pd
-from preprocesamiento.funciones import guardar_tabla
+from preprocesamiento.funciones import buscar_csv, guardar_tabla
 
-''' Convierte la fecha en ISO 8601 de una columna a UNIX timestamp '''
+''' Convierte la columna de fecha a timestamp de todos los archivos csv de una carpeta '''
 
 # PARÁMETROS
-ruta = 'D:/Dropbox/UNI/TFM/datos/4 - Convertir fecha a timestamp/University_of_Pennsylvania_Smell_ID_Test.csv'
-variable_fecha = 'FECHA'
-
-# leer tabla
-tabla = pd.read_csv(ruta, sep=',', float_precision='round_trip')
+ruta_carpeta = 'D:/Dropbox/UNI/TFM/datos/3 - Fecha a timestamp/'
+variable_fecha = 'INFODT'
 
 
-# funcnión para convertir fecha
+# función para convertir fecha
 def convertir_fecha(fecha):
     if pd.isna(fecha):
         return fecha
-    if len(fecha) > 7:  # si tiene hora
-        return int(time.mktime(dt.strptime(fecha, '%m/%Y %H:%M:%S').timetuple()))
-    else:
-        return int(time.mktime(dt.strptime(fecha, '%m/%Y').timetuple()))
+
+    # si el mes está escrito con letras cambiarlo a números
+    if any(c.isalpha() for c in fecha):
+        fecha = fecha.replace(fecha[0:3], meses[fecha[0:3]])
+
+    return int(time.mktime(parse(fecha).timetuple()))
 
 
-# convertir a UNIX
-tabla[variable_fecha] = tabla[variable_fecha].map(convertir_fecha)
+# nombres de meses en español
+locale.setlocale(locale.LC_TIME, 'es')
+meses = {calendar.month_abbr[i][:-1]: str(i) for i in range(1, 13)}
 
-guardar_tabla(tabla, ruta, '_fecha_timestamp')
+# convertir archivos
+for ruta_archivo in buscar_csv(ruta_carpeta):
+    tabla = pd.read_csv(ruta_archivo, sep=',', float_precision='round_trip')
+
+    # si la tabla no tiene fecha, pasar
+    if variable_fecha not in tabla.columns:
+        continue
+
+    # convertir a UNIX timestamp
+    tabla[variable_fecha] = tabla[variable_fecha].map(convertir_fecha)
+
+    guardar_tabla(tabla, ruta_archivo, '_fecha_timestamp')

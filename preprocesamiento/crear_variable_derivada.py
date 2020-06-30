cb6584192d204466da3275f35ec5f23372a9a52a
Andoni <andoni7@outlook.com>
2020-06-30 02:08:53 +0200

    Añadida la reducción de variables del test Benton

--- preprocesamiento/crear_variable_derivada.py
+++ preprocesamiento/crear_variable_derivada.py
@@ -6,7 +6,7 @@ from preprocesamiento.funciones import guardar_tabla
 ''' Genera y añade una variable nueva, derivada a partir de otras de la tabla '''
 
 # PARÁMETROS
-ruta_archivo = 'D:/Dropbox/UNI/TFM/datos/7 - Juntar en un solo archivo/RBD.csv'
+ruta_archivo = 'D:/Dropbox/UNI/TFM/datos/7 - Juntar en un solo archivo/HC.csv'
 
 
 def edad(fila):
@@ -37,20 +37,26 @@ def flag_educacion(fila):
     return int(fila['EDUCYRS'] <= 12)
 
 
-def scopa_preguntas_sexo1(fila):
-    return scopa_preguntas_sexo(fila, 'SCAU22', 'SCAU24')
+def scopa_preguntas_sexo():
+    """ Calcula la puntuación de las preguntas de sexo, independientemente de qué sexo sea """
+    dicts = [{'var_hombre': 'SCAU22', 'var_mujer': 'SCAU24'},
+             {'var_hombre': 'SCAU23', 'var_mujer': 'SCAU25'}]
+    return [tabla.apply(scopa_preguntas_sexo_aux, **variables, axis=1) for variables in dicts]
 
 
-def scopa_preguntas_sexo2(fila):
-    return scopa_preguntas_sexo(fila, 'SCAU23', 'SCAU25')
-
-
-def scopa_preguntas_sexo(fila, var_hombre, var_mujer):
-    """ Calcula la puntuación de la pregunta de sexo, independientemente de qué sexo sea """
+def scopa_preguntas_sexo_aux(fila, var_hombre, var_mujer):
     if pd.isna(fila[var_hombre]) and pd.isna(fila[var_mujer]):
         return np.nan
 
     return fila[var_hombre] if pd.notna(fila[var_hombre]) else fila[var_mujer]
+
+
+def benton_por_pares(fila, v1, v2):
+    """ Calcula la puntuación de los pares de preguntas del test Benton """
+    if pd.isna(fila[v1]) and pd.isna(fila[v2]):
+        return np.nan
+
+    return fila[v1] if pd.notna(fila[v1]) else fila[v2]
 
 
 # leer tabla
@@ -60,7 +66,9 @@ tabla = pd.read_csv(ruta_archivo, sep=',', float_precision='round_trip')
 tabla['age'] = tabla.apply(edad, axis=1)
 tabla['age_dis_onset'] = tabla.apply(edad_de_diagnostico, axis=1)
 tabla['flag_edu'] = tabla.apply(flag_educacion, axis=1)
-tabla['SCAUSEX1'] = tabla.apply(scopa_preguntas_sexo1, axis=1)
-tabla['SCAUSEX2'] = tabla.apply(scopa_preguntas_sexo2, axis=1)
+tabla['SCAUSEX1'], tabla['SCAUSEX2'] = scopa_preguntas_sexo()
+
+for i in range(1, 16):
+    tabla[f'BJLOTPAR{i}'] = tabla.apply(benton_por_pares, v1=f'BJLOT{2 * i - 1}', v2=f'BJLOT{2 * i}', axis=1)
 
 guardar_tabla(tabla, ruta_archivo, '_con_variables_derivadas')

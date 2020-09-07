e07368c9a8da85aac134d795c9de995eda0572cc
Andoni <andoni7@outlook.com>
2020-09-07 17:37:20 +0200

    Añadida función para aplicar regex a una lista de variables

--- preprocesamiento/funciones.py
+++ preprocesamiento/funciones.py
@@ -10,3 +10,13 @@ def guardar_tabla(tabla, ruta, sufijo=''):
     """ Guarda la tabla en la ruta indicada """
     ruta_split = os.path.splitext(ruta)
     tabla.to_csv(ruta_split[0] + sufijo + ruta_split[1], index=False)
+
+
+def aplicar_regex_a_lista(lista_variables, datos):
+    """ Devuelve la lista de variables de los datos que coinciden con el patrón regex de cada variable de la lista """
+
+    variables_encontradas = []
+    for variable in lista_variables:
+        variables_encontradas += datos.loc[:, datos.columns.str.match(variable)].columns.tolist()
+
+    return variables_encontradas
